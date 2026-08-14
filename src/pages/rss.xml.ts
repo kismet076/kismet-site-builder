301931bc84963ea969952085cd4c10f955fcc890
kismet076 <450702330@qq.com>
2026-08-14 17:16:07 +0800

    chore: add src/pages/rss.xml.ts

---
+++ src/pages/rss.xml.ts
@@ -0,0 +1,24 @@
+import rss from '@astrojs/rss';
+import type { APIContext } from 'astro';
+import { getCollection } from 'astro:content';
+import { SITE } from '../config';
+
+export async function GET(context: APIContext) {
+  const posts = (await getCollection('blog', ({ data }) => !data.draft)).sort(
+    (a, b) => b.data.pubDate.valueOf() - a.data.pubDate.valueOf()
+  );
+
+  return rss({
+    title: SITE.name,
+    description: SITE.description,
+    site: context.site ?? SITE.url,
+    items: posts.map((post) => ({
+      title: post.data.title,
+      description: post.data.description,
+      pubDate: post.data.pubDate,
+      link: `/blog/${post.id}/`,
+      categories: [post.data.category, ...post.data.tags],
+    })),
+    customData: `<language>zh-CN</language>`,
+  });
+}
